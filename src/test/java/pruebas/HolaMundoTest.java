589d39dcda9ef24ae48703e46e701730393b5746
manuelsanchez1986 <al03053351@tecmilenio.mx>
2025-07-28 17:55:02 -0600

    Primer commit desde NetBeans

---
+++ src/test/java/pruebas/HolaMundoTest.java
@@ -0,0 +1,12 @@
+package pruebas;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class HolaMundoTest {
+
+    @Test
+    public void pruebaSiemprePasa() {
+        assertTrue(true, "Esta prueba básica siempre pasará");
+    }
+}
